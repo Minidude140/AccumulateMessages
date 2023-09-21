b225cded0e26a995f0a29b6c1b5ab93ec468e3bb
Zac <zac.minidude140@Gmail.com>
2023-09-21 08:40:49 -0600

    Currently working apart from an extra carriage return

--- AccumulateMessages/MessageProgram.vb
+++ AccumulateMessages/MessageProgram.vb
@@ -1,3 +1,13 @@
+'Zachary Christensen
+'RCET 2265
+'Fall 2023
+'Accumulate Messages
+'https://github.com/Minidude140/AccumulateMessages.git
+
+
+
+
+
 
 Imports System
 
@@ -8,8 +18,17 @@ Module MessageProgram
         Test.Auto()
     End Sub
 
-    Function UserMessages(ByVal newMessage As String, ByVal clear As Boolean) As String
+    Function UserMessages(Optional ByVal newMessage As String = " ", Optional ByVal clear As Boolean = False) As String
         'your code here
+        Static messages As String
+
+        If clear Then
+            messages = ""
+        ElseIf newMessage <> "" Then
+            messages = messages & newMessage & vbLf
+        Else newMessage = ""
+            Return messages
+        End If
         Return messages
     End Function
 
